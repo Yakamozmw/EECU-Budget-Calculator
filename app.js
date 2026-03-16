714b4a985b8d38afe8e9e7362c8ee519e8e915e4
Yakamozm <joshuafollente@gmail.com>
2026-03-16 15:48:04 -0700

    more work
    sadge need mreo time

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const familyInputs = document.querySelectorAll('#family input');
 const travelInputs = document.querySelectorAll('#travel input');
 const leisureInputs = document.querySelectorAll('#leisure input');
 const careerSelector = document.querySelector('#careerSelector');
+const allInputs = Array.from(document.querySelectorAll('input')).concat(Array.from(document.querySelectorAll('select')));
 
 const nextBtn = document.querySelector('#nextStep');
+const incomeDisplay = document.querySelector('#Total-Monthly-Income p');
+const expenseDisplay = document.querySelector('#Total-Monthly-Expenses p');
 // variables
 let currentPageIndex = Number(sessionStorage.getItem("currentPageIndex")) || 0;
 let currentField = arrayOfFields[currentPageIndex];
@@ -24,6 +27,7 @@ let leisureExpenses = 0;
 
 // functions
 function saveInputValues() {
+    localStorage.setItem('grossIncome', incomeInputs[0].value);
     localStorage.setItem('careerValue', careerSelector.value);
     let values = [];
     for (let input of incomeInputs) {
@@ -92,6 +96,15 @@ function saveInputValues() {
     localStorage.setItem('leisureValues', JSON.stringify(values));
 }
 
+function saveExpenseValues() {
+    sessionStorage.setItem('householdExpenses', householdExpenses);
+    sessionStorage.setItem('livingExpenses', livingExpenses);
+    sessionStorage.setItem('financeExpenses', financeExpenses);
+    sessionStorage.setItem('familyExpenses', familyExpenses);
+    sessionStorage.setItem('travelExpenses', travelExpenses);
+    sessionStorage.setItem('leisureExpenses', leisureExpenses);
+}
+
 function populateValues() {
     let incomeValues = JSON.parse(localStorage.getItem('incomeValues'));
     let householdValues = JSON.parse(localStorage.getItem('householdValues'));
@@ -140,6 +153,14 @@ function populateValues() {
 }
 
 function calculateIncomeAndExpenses() {
+    householdExpenses = 0;
+    livingExpenses = 0;
+    financeExpenses = 0;
+    familyExpenses = 0;
+    travelExpenses = 0;
+    leisureExpenses = 0;
+    grossIncome = incomeInputs[0].value ? Number(incomeInputs[0].value) : 0;
+    monthlyIncome = ((grossIncome - federalTax(grossIncome))/12).toFixed(2);
     for (let input of householdInputs) {
         if (input.value) {
             householdExpenses += Number(input.value);
@@ -175,7 +196,7 @@ function calculateIncomeAndExpenses() {
 function renderNextField(newPageIndex) {
     for (let selector of arrayOfSelectors) {
         if (selector.classList.contains("selected")) {
-            selector.classList.remove("selected")
+            selector.classList.remove("selected");
         }
         if (selector.getAttribute("navTo") === String(newPageIndex)) {
             selector.classList.add("selected");
@@ -183,7 +204,7 @@ function renderNextField(newPageIndex) {
     }
     for (let field of arrayOfFields) {
         if (!field.classList.contains("hidden")) {
-            field.classList.add("hidden")
+            field.classList.add("hidden");
         }
         arrayOfFields[Number(newPageIndex)].classList.remove("hidden");
     }
@@ -194,6 +215,7 @@ function setUpPageSelector() {
         if (selector.getAttribute("navTo") === "7") {
             selector.addEventListener("click", () => {
                 saveInputValues();
+                saveExpenseValues();
                 window.location.href = 'results.html';
             }); 
         } else {
@@ -206,41 +228,93 @@ function setUpPageSelector() {
     }
 }
 
-function findTaxableIncome() {
+function setUpInputs() {
+    for (let input of allInputs) {
+        input.addEventListener("change", () => {
+            saveInputValues();
+            calculateIncomeAndExpenses();
+            update();
+        });
+    }
+}
 
+function federalTax(incomeAmount) { 
+    let tax = 0;
+    if (incomeAmount <= 12400) {
+        tax = incomeAmount * 0.10;
+    }
+    else if (incomeAmount <= 50400) {
+        tax = (12400 * 0.10) +
+            ((incomeAmount - 12400) * 0.12);
+    }
+    else {
+        tax = (12400 * 0.10) +
+            ((50400 - 12400) * 0.12) +
+            ((incomeAmount - 50400) * 0.22);
+    }
+    return tax;
+}
+
+async function addJobs() {
+    const careerSelect = document.getElementById('careerSelector');
+    const careerSalaryMap = new Map();
+
+    try {
+        const response = await fetch('https://eecu-data-server.vercel.app/data');
+        if (!response.ok) {
+            throw new Error('Network response was not okay');
+        }
+        const users = await response.json();
+
+        users.forEach(user => {
+            careerSalaryMap.set(user["Occupation"], user["Salary"]);
+            const option = new Option(user["Occupation"], user["Occupation"]);
+            careerSelect.add(option);
+        });
+
+        careerSelect.addEventListener('change', () => {
+            incomeInputs[0].value = careerSalaryMap.get(careerSelect.value) || 0;
+        });
+    }
+    catch (error) {
+        console.error(error.message);
+    }
 }
 
 
 // event listeners
 nextBtn.addEventListener("click", () => {
     saveInputValues();
+    calculateIncomeAndExpenses();
+    update();
     // Move to next index & consequently next page
     if (currentPageIndex < 6) {
         renderNextField(Number(currentPageIndex) + 1);
         currentPageIndex = currentPageIndex + 1;
     } else if (currentPageIndex >= 6) {
+        saveExpenseValues();
         window.location.href = 'results.html';
     }
 })
 
 // initialization
 populateValues();
+addJobs();
 calculateIncomeAndExpenses();
 renderNextField(currentPageIndex);
 setUpPageSelector();
+setUpInputs();
 
 // Pie Chart
-const inputs = document.querySelectorAll("input" || "select");
 const canvas = document.querySelector('#chart');
 let currentChart = null;
-
-document.body.addEventListener('input', () => {
-    update();
-});
 
 update();
 
 function update() {
+    incomeDisplay.textContent = `$${monthlyIncome}`;
+    expenseDisplay.textContent = `$${householdExpenses + livingExpenses + financeExpenses + familyExpenses + travelExpenses + leisureExpenses}`;
+
     currentChart?.destroy();
     currentChart = new Chart(canvas, {
         type: 'doughnut',
@@ -262,7 +336,7 @@ function update() {
             datasets: [
                 {
                     label: 'Monthly (USD)',
-                    data: [householdExpenses, livingExpenses, financeExpenses, travelExpenses, leisureExpenses]
+                    data: [householdExpenses, livingExpenses, financeExpenses, familyExpenses, travelExpenses, leisureExpenses]
                 }
             ]
         }
--- results.js
+++ results.js
@@ -1,4 +1,10 @@
 const arrayOfSelectors = document.querySelector(".Page-Selector").children;
+let householdExpenses = JSON.parse(sessionStorage.getItem('householdExpenses')) || 0;
+let livingExpenses = JSON.parse(sessionStorage.getItem('livingExpenses')) || 0;
+let financeExpenses = JSON.parse(sessionStorage.getItem('financeExpenses')) || 0;
+let familyExpenses = JSON.parse(sessionStorage.getItem('familyExpenses')) || 0;
+let travelExpenses = JSON.parse(sessionStorage.getItem('travelExpenses')) || 0;
+let leisureExpenses = JSON.parse(sessionStorage.getItem('leisureExpenses')) || 0;
 
 function setUpPageSelector() {
     for (const selector of arrayOfSelectors) { 
@@ -12,3 +18,37 @@ function setUpPageSelector() {
 }
 
 setUpPageSelector();
+
+const canvas = document.querySelector('#chartResultPg');
+let currentChart = null;
+
+chartUpdate();
+
+function chartUpdate() {
+    currentChart?.destroy();
+    currentChart = new Chart(canvas, {
+        type: 'doughnut',
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: 'Your Monthly Expenses'
+                }
+            }
+
+        },
+        data: {
+            labels: ['Household', 'Living', 'Finance', 'Family', 'Travel', 'Leisure'],
+            datasets: [
+                {
+                    label: 'Monthly (USD)',
+                    data: [householdExpenses, livingExpenses, financeExpenses, familyExpenses, travelExpenses, leisureExpenses]
+                }
+            ]
+        }
+    });
+}
